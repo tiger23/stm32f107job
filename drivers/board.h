73021e13161eb739b46b6aa1cf5fb252b007fd63
tiger23 <tiger.w0412@gmail.com>
2014-08-16 07:23:17 +0800

    增加CAN文件，修改borard.h,rtconfig.py

--- drivers/board.h
+++ drivers/board.h
@@ -41,6 +41,8 @@
 #define STM32_SRAM_END          (0x20000000 + STM32_SRAM_SIZE * 1024)
 
 #define RT_USING_UART1
+#define RT_USING_UART2
+//#define RT_USING_UART3
 #define RT_USING_SPI1
 
 void rt_hw_board_init(void);
--- drivers/can_network.c
+++ drivers/can_network.c
@@ -16,7 +16,7 @@
 #include "stm32f10x.h"
 #include <stdio.h>
 #include "bsp_button.h"
-#include "bsp_timer.h"
+//#include "bsp_timer.h"
 #include "bsp_led.h"
 #include "can_network.h"
 
@@ -25,6 +25,17 @@ __IO uint32_t CANBus_flag;
 CanRxMsg g_CanRxMessage;
 CanTxMsg g_CanTxMessage;
 
+/* Private define ------------------------------------------------------------*/
+
+#define CAN_BAUDRATE  1000      /* 1MBps   */
+/* #define CAN_BAUDRATE  500*/  /* 500kBps */
+/* #define CAN_BAUDRATE  250*/  /* 250kBps */
+/* #define CAN_BAUDRATE  125*/  /* 125kBps */
+/* #define CAN_BAUDRATE  100*/  /* 100kBps */
+/* #define CAN_BAUDRATE  50*/   /* 50kBps  */
+/* #define CAN_BAUDRATE  20*/   /* 20kBps  */
+/* #define CAN_BAUDRATE  10*/   /* 10kBps  */
+/* Private macro -------------------------------------------------------------*/
 
 /*
  * 函数名：CAN_GPIO_Config
@@ -37,23 +48,35 @@ static void CAN_GPIO_Config(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
 
-    /*外设时钟设置*/
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOB, ENABLE);
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1, ENABLE);
+    /* Configure CAN1 and CAN2 IOs **********************************************/
+    /* GPIOB, GPIOD and AFIO clocks enable */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO | RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOB, ENABLE);
 
-    /*IO设置*/
-    GPIO_PinRemapConfig(GPIO_Remap1_CAN1, ENABLE);
-    /* Configure CAN pin: RX */                                                // PB8
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;                // 上拉输入
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
-    /* Configure CAN pin: TX */                                                // PB9
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;              // 复用推挽输出
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    /* Configure CAN1 RX pin */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    /* Configure CAN2 RX pin */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
 
+    /* Configure CAN1 TX pin */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
+
+    /* Configure CAN2 TX pin */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+    /* Remap CAN1 and CAN2 GPIOs */
+    GPIO_PinRemapConfig(GPIO_Remap2_CAN1 , ENABLE);
+    GPIO_PinRemapConfig(GPIO_Remap_CAN2, ENABLE);
+    /* Configure CAN1 and CAN2 **************************************************/
+    /* CAN1 and CAN2 Periph clocks enable */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1 | RCC_APB1Periph_CAN2, ENABLE);
 }
 
 /*
@@ -69,10 +92,14 @@ static void CAN_NVIC_Config(void)
     /* Configure one bit for preemption priority */
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
     /*中断设置*/
-    NVIC_InitStructure.NVIC_IRQChannel = USB_LP_CAN1_RX0_IRQn;     //CAN1 RX0中断
+    NVIC_InitStructure.NVIC_IRQChannel = CAN1_RX0_IRQn;     //CAN1 RX0中断
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;          //抢占优先级0
     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;             //子优先级为3
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    NVIC_InitStructure.NVIC_IRQChannel = CAN2_RX0_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x1;
     NVIC_Init(&NVIC_InitStructure);
 }
 
@@ -88,22 +115,49 @@ static void CAN_Mode_Config(void)
     CAN_InitTypeDef        CAN_InitStructure;
     /************************CAN通信参数设置**********************************/
     /*CAN寄存器初始化*/
+    /* CAN1 and CAN2 register init */
     CAN_DeInit(CAN1);
+    CAN_DeInit(CAN2);
+
+    /* Struct init*/
     CAN_StructInit(&CAN_InitStructure);
-    /*CAN单元初始化*/
-    CAN_InitStructure.CAN_TTCM = DISABLE;          //MCR-TTCM  关闭时间触发通信模式使能
-    CAN_InitStructure.CAN_ABOM = ENABLE;           //MCR-ABOM  自动离线管理
-    CAN_InitStructure.CAN_AWUM = ENABLE;           //MCR-AWUM  使用自动唤醒模式
-    CAN_InitStructure.CAN_NART = DISABLE;          //MCR-NART  禁止报文自动重传   DISABLE-自动重传
-    CAN_InitStructure.CAN_RFLM = DISABLE;          //MCR-RFLM  接收FIFO 锁定模式  DISABLE-溢出时新报文会覆盖原有报文
-    CAN_InitStructure.CAN_TXFP = DISABLE;          //MCR-TXFP  发送FIFO优先级 DISABLE-优先级取决于报文标示符
-    //CAN_InitStructure.CAN_Mode = CAN_Mode_Normal;  //正常工作模式
-    CAN_InitStructure.CAN_Mode = CAN_Mode_LoopBack;
-    CAN_InitStructure.CAN_SJW = CAN_SJW_2tq;       //BTR-SJW 重新同步跳跃宽度 2个时间单元
-    CAN_InitStructure.CAN_BS1 = CAN_BS1_6tq;       //BTR-TS1 时间段1 占用了6个时间单元
-    CAN_InitStructure.CAN_BS2 = CAN_BS2_3tq;       //BTR-TS1 时间段2 占用了3个时间单元
-    CAN_InitStructure.CAN_Prescaler = 4;       ////BTR-BRP 波特率分频器  定义了时间单元的时间长度 36/(1+6+3)/4=0.8Mbps
+
+    /* CAN1 and CAN2  cell init */
+    CAN_InitStructure.CAN_TTCM = DISABLE;
+    CAN_InitStructure.CAN_ABOM = DISABLE;
+    CAN_InitStructure.CAN_AWUM = DISABLE;
+    CAN_InitStructure.CAN_NART = DISABLE;
+    CAN_InitStructure.CAN_RFLM = DISABLE;
+    CAN_InitStructure.CAN_TXFP = ENABLE;
+    CAN_InitStructure.CAN_Mode = CAN_Mode_Normal;
+    CAN_InitStructure.CAN_SJW = CAN_SJW_1tq;
+    CAN_InitStructure.CAN_BS1 = CAN_BS1_3tq;
+    CAN_InitStructure.CAN_BS2 = CAN_BS2_2tq;
+
+#if CAN_BAUDRATE == 1000 /* 1MBps */
+    CAN_InitStructure.CAN_Prescaler = 6;
+#elif CAN_BAUDRATE == 500 /* 500KBps */
+    CAN_InitStructure.CAN_Prescaler = 12;
+#elif CAN_BAUDRATE == 250 /* 250KBps */
+    CAN_InitStructure.CAN_Prescaler = 24;
+#elif CAN_BAUDRATE == 125 /* 125KBps */
+    CAN_InitStructure.CAN_Prescaler = 48;
+#elif  CAN_BAUDRATE == 100 /* 100KBps */
+    CAN_InitStructure.CAN_Prescaler = 60;
+#elif  CAN_BAUDRATE == 50 /* 50KBps */
+    CAN_InitStructure.CAN_Prescaler = 120;
+#elif  CAN_BAUDRATE == 20 /* 20KBps */
+    CAN_InitStructure.CAN_Prescaler = 300;
+#elif  CAN_BAUDRATE == 10 /* 10KBps */
+    CAN_InitStructure.CAN_Prescaler = 600;
+#else
+#error "Please select first the CAN Baudrate in Private defines in main.c "
+#endif  /* CAN_BAUDRATE == 1000 */
+
+    /*Initializes the CAN1  and CAN2 */
     CAN_Init(CAN1, &CAN_InitStructure);
+    CAN_Init(CAN2, &CAN_InitStructure);
+
 }
 
 /*
@@ -117,23 +171,24 @@ static void CAN_Filter_Config(void)
 {
     CAN_FilterInitTypeDef  CAN_FilterInitStructure;
 
-    /*CAN过滤器初始化*/
-    CAN_FilterInitStructure.CAN_FilterNumber = 0;                   //过滤器组0
-    CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask; //工作在标识符屏蔽位模式
-    CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit; //过滤器位宽为单个32位。
-    /* 使能报文标示符过滤器按照标示符的内容进行比对过滤，扩展ID不是如下的就抛弃掉，是的话，会存入FIFO0。 */
-
-    CAN_FilterInitStructure.CAN_FilterIdHigh = (((u32)0x1314 << 3) & 0xFFFF0000) >> 16;         //要过滤的ID高位
-    CAN_FilterInitStructure.CAN_FilterIdLow = (((u32)0x1314 << 3) | CAN_ID_EXT | CAN_RTR_DATA) & 0xFFFF; //要过滤的ID低位
-    CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0xFFFF;          //过滤器高16位每位必须匹配
-    CAN_FilterInitStructure.CAN_FilterMaskIdLow = 0xFFFF;           //过滤器低16位每位必须匹配
-    CAN_FilterInitStructure.CAN_FilterFIFOAssignment = CAN_Filter_FIFO0 ;           //过滤器被关联到FIFO0
-    CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;          //使能过滤器
+    /* CAN1 filter init */
+    CAN_FilterInitStructure.CAN_FilterNumber = 1;
+    CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask;
+    CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit;
+    CAN_FilterInitStructure.CAN_FilterIdHigh = 0x6420;
+    CAN_FilterInitStructure.CAN_FilterIdLow = 0x0000;
+    CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0x0000;
+    CAN_FilterInitStructure.CAN_FilterMaskIdLow = 0x0000;
+    CAN_FilterInitStructure.CAN_FilterFIFOAssignment = 0;
+    CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;
     CAN_FilterInit(&CAN_FilterInitStructure);
-    /*CAN通信中断使能*/
-    CAN_ITConfig(CAN1, CAN_IT_FMP0, ENABLE);
-}
 
+    /* CAN2 filter init */
+    CAN_FilterInitStructure.CAN_FilterIdHigh = 0x2460;
+    CAN_FilterInitStructure.CAN_FilterNumber = 15;
+    CAN_FilterInit(&CAN_FilterInitStructure);
+
+}
 
 /*
  * 函数名：CAN_Config
@@ -148,6 +203,13 @@ void CAN_Config(void)
     CAN_NVIC_Config();
     CAN_Mode_Config();
     CAN_Filter_Config();
+    /*CAN通信中断使能*/
+    /* IT Configuration for CAN1 */
+    CAN_ITConfig(CAN1, CAN_IT_FMP0, ENABLE);
+
+    /* IT Configuration for CAN2 */
+    CAN_ITConfig(CAN2, CAN_IT_FMP0, ENABLE);
+
 }
 
 
@@ -162,7 +224,7 @@ void CAN_SetMsg(void)
 {
     //g_CanTxMessage.StdId=0x00;
     g_CanTxMessage.ExtId = 0x1314;                  //使用的扩展ID
-    g_CanTxMessage.IDE = CAN_ID_EXT;                //扩展模式
+    g_CanTxMessage.IDE = CAN_ID_STD;                //扩展模式
     g_CanTxMessage.RTR = CAN_RTR_DATA;              //发送的是数据
     g_CanTxMessage.DLC = 2;                         //数据长度为2字节
     g_CanTxMessage.Data[0] = 0xAB;
@@ -180,10 +242,10 @@ u8 CAN_Send_Msg(u8 *msg, u8 len)
 {
     u8 mbox;
     u16 i = 0;
-    //CanTxMsg g_CanTxMessage;
-    g_CanTxMessage.StdId = 0x1314;                  // 标准标识符为0
+    
+    g_CanTxMessage.StdId = 0x1314;                  // 标准标识符为0x1314
     g_CanTxMessage.ExtId = 0x1314;              // 设置扩展标示符（29位）
-    g_CanTxMessage.IDE = CAN_ID_EXT;        // 使用扩展标识符
+    g_CanTxMessage.IDE = CAN_ID_STD;        // 使用标准标识符
     g_CanTxMessage.RTR = CAN_RTR_DATA;      // 消息类型为数据帧，一帧8位
     g_CanTxMessage.DLC = len;                           // 发送两帧信息
     for (i = 0; i < len; i++)
@@ -219,22 +281,48 @@ u8 CAN_Receive_Msg(u8 *buf)
 /*  available peripheral interrupt handler's name please refer to the startup */
 /*  file (startup_stm32f10x_xx.s).                                            */
 /******************************************************************************/
-void USB_LP_CAN1_RX0_IRQHandler(void)
-{
 
-    /*从邮箱中读出报文*/
+/**
+  * @brief  This function handles CAN1 RX0 Handler.
+  * @param  None
+  * @retval None
+  */
+void CAN1_RX0_IRQHandler(void)
+{
     CAN_Receive(CAN1, CAN_FIFO0, &g_CanRxMessage);
 
-    /* 比较ID和数据是否为0x1314及DCBA */
-    if ((g_CanRxMessage.ExtId == 0x1314) && (g_CanRxMessage.IDE == CAN_ID_EXT)
-            && (g_CanRxMessage.DLC == 2) && ((g_CanRxMessage.Data[1] | g_CanRxMessage.Data[0] << 8) == 0xDCBA))
-    {
-        CANBus_flag = 0;                           //接收成功
-        rt_kprintf("CAN bus running.");
+  if ((g_CanRxMessage.StdId == 0x321)&&(g_CanRxMessage.IDE == CAN_ID_STD)&&(g_CanRxMessage.DLC == 1)&&(g_CanRxMessage.Data[0] == 0xAA))
+  {
+    /* Turn On LED3 */
+    //LED_Display(0x03); /* OK */
     }
     else
     {
-        CANBus_flag = 0xff;                        //接收失败
-    }
+    /* Turn Off LED3 */
+    //LED_Display(0x05); /* Error */
 }
+}
+
+/**
+  * @brief  This function handles CAN2 RX0 Handler.
+  * @param  None
+  * @retval None
+  */
+
+void CAN2_RX0_IRQHandler(void)
+{
+  CAN_Receive(CAN2, CAN_FIFO0, &g_CanRxMessage);
+
+  if ((g_CanRxMessage.StdId == 0x321)&&(g_CanRxMessage.IDE == CAN_ID_STD)&&(g_CanRxMessage.DLC == 1)&&(g_CanRxMessage.Data[0] == 0x55))
+  {
+    /* Turn On LED4 */
+    //LED_Display(0x04); /* OK */
+  }
+  else
+  {
+    /* Turn Off LED4 */
+    //LED_Display(0x06); /* Error */
+  }
+}
+
 /**************************END OF FILE************************************/
